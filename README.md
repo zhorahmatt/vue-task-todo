2454dd5395620bc72b1f136eb7b117eb51cca038
zhorahmatt <info.rahmathidayat@gmail.com>
2025-05-14 20:26:55 +0800

    wip page initial for vue app task app

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Vue 3 + TypeScript + Vite
+# Vue Task Todo
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+This project is a learning phase for me to learn front-end development with Vue.js and TypeScript.
 
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+Nothing fancy, just a simple todo list with a task description and a checkbox.
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script lang="ts" setup>
+import { ref } from 'vue';
 
+const message = ref('Task App');
+</script>
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>{{ message }}</h1>
+    <form>
+      <label for="">
+        New Task
+        <input name="newTask">
+      </label>
+    </form>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+main {
+  max-width: 800px;
+  margin: 1rem auto;
 }
 </style>
